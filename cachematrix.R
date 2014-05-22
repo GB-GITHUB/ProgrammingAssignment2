82621a623dabf17337b9725cf6b8216384684690
GB-GITHUB <gb.github@gmail.com>
2014-05-22 16:37:21 +0200

    daily dev

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,68 @@
-# makeVector <- function(x = numeric()) {
-#         m <- NULL
-#         set <- function(y) {
-#                 x <<- y
-#                 m <<- NULL
-#         }
-#         get <- function() x
-#         setmean <- function(mean) m <<- mean
-#         getmean <- function() m
-#         list(set = set, get = get,
-#              setmean = setmean,
-#              getmean = getmean)
-# }
-
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# makeCacheMatrix: Creates a special "matrix" object that can cache its inverse
+# INPUT: a matrix x
+# OUTPUT: a "special" matrix with methods
 makeCacheMatrix <- function(x = matrix()) {
+        cInvMatrix <- NULL
 
+        # method setMatrix
+        setMatrix <- function(matrix) {
+                x <<- matrix
+                cInvMatrix <- NULL
+        }
+
+        # method getMatrix
+        getMatrix <- function() {x}
+        
+        # method setInvMatrix
+        setInvMatrix <- function(i_matrix) {cInvMatrix<<-i_matrix}
+        
+        # method getInvMatrix
+        getInvMatrix <- function() {cInvMatrix}
+
+        # Return "special" matrix
+        list(setMatrix=setMatrix, getMatrix=getMatrix,
+             setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve: 
+#        Computes he inverse of the special matrix returned by makeCacheMatrix.
+#        If inverse has already been calculated,, then cacheSolve retrieve the inverse from the cache
+# INPUT: a matrix x
+# OUTPUT: a "special" matrix with methods
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## retrieved cached matrices
+        cInvMatrix <- x$getInvMatrix()
+        cMatrix <- x$getMatrix()
+        print(cMatrix)
+        print(cInvMatrix)
+        
+        ## create an identity matrix with same size as given matrix
+        idMatrix = diag(ncol(cMatrix))
+        print(idMatrix)
+        print(!is.null(cInvMatrix))
+        print(dim(cMatrix) == dim(cInvMatrix))
+        print(cMatrix %*% cInvMatrix)
+        print(all((cMatrix %*% cInvMatrix) == idMatrix))
+
+        ## Check if cached matrix exist
+        ## Check if cached Matrix has same size than matrix to solve
+        ## check if dot product of given matrix and cached inverse matrix gives an identity matrix
+        if(!is.null(cInvMatrix) && dim(cMatrix) == dim(cInvMatrix) && all((cMatrix %*% cInvMatrix) == idMatrix)) {
+                message("cached inverse matrix available")
+                return(cInvMatrix)
+        } else{
+                message("cached inverse matrix does not exist")                
+        }
+
+        # Solve inverse matrix
+        cInvMatrix <- solve(x$getMatrix(), ...)
+        # Cached inverse matrix
+        x$setInvMatrix(cInvMatrix)
+        # Return inverse matrix
+        cInvMatrix
 }
