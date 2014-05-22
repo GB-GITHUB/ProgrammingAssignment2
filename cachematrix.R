6cb43eb71027948a60c29a9cad6be5ad7d43355d
GB-GITHUB <gb.github@gmail.com>
2014-05-22 11:07:46 +0200

    Adding code example as reference

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,17 @@
+# makeVector <- function(x = numeric()) {
+#         m <- NULL
+#         set <- function(y) {
+#                 x <<- y
+#                 m <<- NULL
+#         }
+#         get <- function() x
+#         setmean <- function(mean) m <<- mean
+#         getmean <- function() m
+#         list(set = set, get = get,
+#              setmean = setmean,
+#              getmean = getmean)
+# }
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
